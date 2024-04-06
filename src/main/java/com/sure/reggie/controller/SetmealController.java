ae0333209682e4514dab59d8f2c49f0800982fb2
Sure <pitipalaa@gmail.com>
2024-04-06 13:25:17 +0800

    Enhance the functionality to query Setmeals based on conditions.

--- src/main/java/com/sure/reggie/controller/SetmealController.java
+++ src/main/java/com/sure/reggie/controller/SetmealController.java
@@ -103,9 +103,14 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
-    public Result<List<Setmeal>> list(@RequestBody Setmeal setmeal) {
-        
-        return null;
+    public Result<List<Setmeal>> list(Setmeal setmeal) {
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(setmeal.getCategoryId() != null, Setmeal::getCategoryId, setmeal.getCategoryId());
+        queryWrapper.eq(setmeal.getStatus() != null, Setmeal::getStatus, setmeal.getStatus());
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        List<Setmeal> list = setmealService.list(queryWrapper);
+        return Result.success(list);
     }
 
 }
